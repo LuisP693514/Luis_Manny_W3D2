34c0cee8f76fa2041827eed59efcf480201c2e4b
Luis <perezluis167@gmail.com>
2022-11-22 17:26:38 -0500

    almost done

--- Memory_Puzzle/game.rb
+++ Memory_Puzzle/game.rb
@@ -1,6 +1,26 @@
-
-#Game class
-
-class Game 
-
+#game class
+class Game
+    def initialize
+        @board = Board.new(4)
+        @prev_position = []
+    end
+    def play
+        while !@board.won?
+            @board.render
+            puts “Enter Position like 1 2”
+            input = gets.chomp.split.map(&:to_i)
+            make_guess(input)
+            sleep(2)
+            system(“clear”)
+        end
+    end
+    def make_guess(pos)
+        if @prev_position.length == 0
+            @board.reveal(pos)
+            @prev_position = pos
+        else
+            @board.grid[pos[0]][pos[1]]
+        end
+    end
 end
+
