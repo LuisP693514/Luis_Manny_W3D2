ba2b4872fd017a9b8527d479ecfc6c2b15b2be4c
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-11-22 14:38:37 -0500

    after populate

--- Memory_Puzzle/board.rb
+++ Memory_Puzzle/board.rb
@@ -2,9 +2,38 @@
 require_relative "card.rb"
 
 class Board
+    attr_reader :grid
+    def initialize(num=4)
+        
+        @grid = Array.new(num) { Array.new(num) }
 
-    def initialize
+    end
 
-        @grid
+    def populate
+        (@grid.length / 2).times do
+            values = (0..9).to_a
+            random = rand(0..9)
+            card_arr = []
+            2.times do
+                card_arr << Card.new(values[random])
+            end
+            while card_arr.length > 0
+                @grid.each.with_index do |row, idx1|
+                    row.each.with_index do |value, idx2|
+                        if value == nil
+                            @grid[idx1][idx2] = card_arr.pop
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+
 
 end
+
+
+b = Board.new(4)
+b.populate
+print b.grid
--- Memory_Puzzle/card.rb
+++ Memory_Puzzle/card.rb
@@ -2,13 +2,19 @@
 
 class Card
 
-    def initialize
-
+    def initialize(value)
+        @value = value
         @face_value = false
 
     end
 
     def hide
+        @face_value = false
+    end
+
+    def reveal
+        @face_value = true
+    end
 
 end
 
