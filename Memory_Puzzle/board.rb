e405d2a4a02a736c1212eb3b1fec8e6e82aab4bc
Luis <perezluis167@gmail.com>
2022-11-22 15:32:20 -0500

    finished populate for real this time

--- Memory_Puzzle/board.rb
+++ Memory_Puzzle/board.rb
@@ -14,30 +14,34 @@ class Board
 
     def populate
         (@grid.length**2/2).times do 
-            values = (0..9).to_a
-            random = rand(0..9)
+            values = (0..@grid.length**2/2).to_a
+            random = rand(0..@grid.length**2/2)
             card_arr = []
             2.times {card_arr << Card.new(values[random]).value }
                 
             while card_arr.length > 0
                 @grid.each.with_index do |row, idx1|
-
                     row.each.with_index do |value, idx2|
                         if value == nil
                             @grid[idx1][idx2] = card_arr.pop
                         end
                     end
-                    
                 end
             end
         end
+
+        100.times do
+            @grid.each {|row| row.shuffle!}
+        end
+
     end
 
+    
 
 
 end
 
 
-b = Board.new(4)
+b = Board.new(10)
 b.populate
 p b.grid
